9f13f79e9db6733b7a68864832247dad6c1b440e
Eshika Shah <63183513+EshikaShah@users.noreply.github.com>
2022-04-06 22:52:42 +0530

    Time: 169 ms (49.42%), Space: 13.9 MB (92.44%) - LeetHub

--- 923-3sum-with-multiplicity/923-3sum-with-multiplicity.py
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.py
@@ -1,22 +1,28 @@
 class Solution:
-    def threeSumMulti(self, arr: List[int], target: int) -> int:
-        arr.sort()
+    def threeSumMulti(self, A: List[int], target: int) -> int:
+        MOD = 1_000_000_007
+        seen = Counter(A)
+        A = list(set(A))
+        A.sort()
+        n = len(A)
         ans = 0
-        for i in range(2, len(arr)):
-            j, k = 0, i - 1
-            while j < k:
-                sm = arr[i] + arr[j] + arr[k]
-                if sm < target:
-                    j += 1
-                elif sm > target:
-                    k -= 1
-                else:
-                    l = r = 1
-                    while j + l < k and arr[j] == arr[j + l]:
-                        l += 1
-                    while j + l <= k - r and arr[k] == arr[k - r]:
-                        r += 1
-                    ans += (l + r) * (l + r - 1) // 2 if arr[j] == arr[k] else l * r
-                    j += l;
-                    k -= r;
-        return ans % (10 ** 9 + 7)
+        for i in range(n):
+            need = target - A[i]
+            obs = set()
+            for x in A[i:]:
+                obs.add(need - x)
+                if x > need:
+                    break
+                if x in obs:
+                    a, b, c = sorted([A[i], x, need - x])
+                    if a < b < c:
+                        ans += seen[a] * seen[b] * seen[c]
+                    if a == b < c:
+                        ans += seen[a] * (seen[a] - 1) * seen[c] // 2
+                    if a < b == c:
+                        ans += seen[a] * seen[b] * (seen[b] - 1) // 2
+                    if a == b == c:
+                        ans += seen[a] * (seen[a] - 1) * (seen[a] - 2) // 6
+                    ans %= MOD
+                    
+        return ans
