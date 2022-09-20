4d1c7968d6ddb348119029817083dbfb4a4c64d7
Eshika Shah <63183513+EshikaShah@users.noreply.github.com>
2022-09-20 16:57:30 +0530

    Time: 2903 ms (69.87%), Space: 34.3 MB (9.96%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.py
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.py
@@ -7,17 +7,16 @@ class Solution:
         d = Counter(changed)
         changed.sort(reverse = True)
         for num in changed:
-            if(num%2==0 and (num//2 in d)):
-                if(d[num]!=0 and d[num//2]!=0):
-                    if(num == 0):
-                        if(d[num]%2 == 0):
-                            d[num] -= 1
-                            d[num//2] -= 1
-                            l.append(num//2)
-                    else:
+            if(num%2==0 and d[num]!=0 and d[num//2]!=0):
+                if(num == 0):
+                    if(d[num]%2 == 0):
                         d[num] -= 1
                         d[num//2] -= 1
                         l.append(num//2)
+                else:
+                    d[num] -= 1
+                    d[num//2] -= 1
+                    l.append(num//2)
         if(sum(d.values())==0):
             return l
         return []
