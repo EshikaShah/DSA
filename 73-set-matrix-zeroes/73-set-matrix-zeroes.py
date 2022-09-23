9ab29deb9f9a07c295a694d407e76eed5e4c5e2a
Eshika Shah <63183513+EshikaShah@users.noreply.github.com>
2022-09-24 00:43:35 +0530

    Time: 313 ms (13.47%), Space: 14.6 MB (90.51%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.py
@@ -3,16 +3,31 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        row = []
-        col = []
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
-                if(matrix[i][j]==0):
-                    row.append(i)
-                    col.append(j)
-        for i in row:
-            for j in range(len(matrix[0])):
-                matrix[i][j] = 0
-        for j in col:
-            for i in range(len(matrix)):
-                matrix[i][j] = 0
+        m = len(matrix)
+        n = len(matrix[0])
+		
+        first_row_has_zero = False
+        first_col_has_zero = False
+        
+        for row in range(m):
+            for col in range(n):
+                if matrix[row][col] == 0:
+                    if row == 0:
+                        first_row_has_zero = True
+                    if col == 0:
+                        first_col_has_zero = True
+                    matrix[row][0] = matrix[0][col] = 0
+    
+        for row in range(1, m):
+            for col in range(1, n):
+                if matrix[0][col] == 0 or matrix[row][0] == 0:
+                    matrix[row][col] = 0 
+        
+        # update the first row and col if they're zero
+        if first_row_has_zero:
+            for col in range(n):
+                matrix[0][col] = 0
+        
+        if first_col_has_zero:
+            for row in range(m):
+                matrix[row][0] = 0
